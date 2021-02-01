c4668e24197e1ff271d5985e9d0ae119d997a1a5
Nicole Agila <nagila@espol.edu.ec>
2021-02-01 15:59:17 -0500

    add facebook comments to sentiment analysis

--- analysis/graph_analysis.py
+++ analysis/graph_analysis.py
@@ -11,28 +11,25 @@ df_tweets = df_tweets.apply(lambda x : None if x["mencions"]=="[]" else x ,axis=
 df_tweets = df_tweets.dropna()
 # %%
 df_tweets["mencions"]=df_tweets["mencions"].apply(lambda x: x.replace("[","").replace("]","").replace("'","").split(",") )
-#%%
-'''df_tweets.head()'''
+
 # %%
-'''menciones_enlista = df_tweets["mencions"]'''
-#%%
-'''menciones = set()
-for mencion_to in menciones_enlista:
-    for j in mencion_to:
-        menciones.add(j.strip())
-menciones=list(menciones)'''
-# %%
-g = nx.DiGraph()
+'''raw_edges=open("../data/edges.csv","w")
+raw_edges.write("mencion,menciona_a\n")
 #%%
 for user in df_tweets["username"].unique():
     mencion_row=df_tweets[df_tweets["username"]==user]["mencions"]
     for mencion_lista in mencion_row:
         for mencion_to in mencion_lista:
-            g.add_edge(user,mencion_to.strip(), weigth=1)
+            raw_edges.write(user+","+mencion_to.strip()+"\n")
+            raw_edges.close()
+print("Fin de archivo")'''
 #%%
-plt.hist([v for k,v in nx.degree(g)]);
-plt.show()
+egdes = open("../data/graph.csv","r")
+g = nx.DiGraph()
+for linea in egdes:
+    source,target,tipo,identificacion,label,time,weight = linea.split(",")
+    g.add_edge(source,target, weigth=weight)
 # %%
-nx.draw(g, node_size=2)
+nx.draw(g, node_size=5,with_labels=True)
 plt.show()
 # %%
