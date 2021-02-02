7dae1f62dd7b05e69ab7cd814fd9a31081e7d275
sbenalca <37081244+sbenalca@users.noreply.github.com>
2021-02-01 20:43:27 -0500

    Analisis realizado HeatMaps

--- analysis/analysis_instagram.py
+++ analysis/analysis_instagram.py
@@ -4,6 +4,8 @@ from ast import literal_eval
 from utils import bar_plot, pie_sentiment_analysis, get_wordcloud
 import matplotlib.pyplot as plt
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import seaborn as sns
+import numpy as np
 
 
 # dfDestacados = pd.read_csv("data/instagramDestacados.csv")
@@ -38,8 +40,8 @@ dfRecientesEn = pd.read_csv("data/insta_test_traducido_Recientes.csv")
 sid = SentimentIntensityAnalyzer()
 dfDestacadosEn['texto'] = dfDestacadosEn['texto'].astype(str)
 dfRecientesEn['texto'] = dfRecientesEn['texto'].astype(str)
-dfDestacadosEn['fecha'] = pd.to_datetime(dfDestacadosEn['fecha'])
-dfRecientesEn['fecha'] = pd.to_datetime(dfRecientesEn['fecha'])
+dfDestacadosEn['fechaFormat'] = pd.to_datetime(dfDestacadosEn['fecha'])
+dfRecientesEn['fechaFormat'] = pd.to_datetime(dfRecientesEn['fecha'])
 
 
 
@@ -48,15 +50,43 @@ print(dfDestacadosEn)
 dfRecientesEn["sentimiento"] = dfRecientesEn['texto'].apply(lambda i: sid.polarity_scores(i)["compound"])
 print(dfRecientesEn)
 
-df_insta_Destacados = dfDestacadosEn.copy()
-df_insta_Destacados['dia'] = df_insta_Destacados["fecha"].dt.day
-# group by month and year, get the average
-df_insta_Destacados.to_csv("InstagramDestacadosUnicos.csv")
-df_insta_Destacados = df_insta_Destacados.groupby(['dia'])
 
-df_insta_Recientes = dfRecientesEn.copy()
-df_insta_Recientes['dia'] = df_insta_Recientes["fecha"].dt.day
-# group by month and year, get the average
-df_insta_Recientes.to_csv("InstagramRecientesUnicos.csv")
-df_insta_Recientes = df_insta_Recientes.groupby(['dia'])
+
+df_insta_Destacados = dfDestacadosEn.copy()[["fecha","fechaFormat", "sentimiento"]]
+df_insta_Destacados['hora'] = df_insta_Destacados["fechaFormat"].dt.hour
+df_insta_Destacados['dia'] = df_insta_Destacados["fechaFormat"].dt.day
+df_insta_Destacados['mes'] = df_insta_Destacados["fechaFormat"].dt.month
+
+df_insta_Recientes = dfRecientesEn.copy()[["fecha","fechaFormat", "sentimiento"]]
+df_insta_Recientes['hora'] = df_insta_Recientes["fechaFormat"].dt.hour
+df_insta_Recientes['dia'] = df_insta_Recientes["fechaFormat"].dt.day
+df_insta_Recientes['mes'] = df_insta_Recientes["fechaFormat"].dt.month
+
+
+# df_insta_Destacados.to_csv("InstagramDestacadosUnicos.csv")
+del df_insta_Destacados["fechaFormat"]
+
+# df_insta_Recientes.to_csv("InstagramRecientesUnicos.csv")
+del df_insta_Recientes["fechaFormat"]
+
+df_insta_Destacados.fecha=df_insta_Destacados.fecha.str[7:19]
+
+df_insta_Recientes.fecha=df_insta_Recientes.fecha.str[7:19]
+
+
+instaDestacados = df_insta_Destacados.pivot(index='fecha', columns='mes', values='sentimiento')
+instd = sns.heatmap(instaDestacados, vmin=-1, vmax=1)
+instd.figure.savefig("HeatMapPostDestacadosPorDia.png")
+
+
+
+instaRecientes = df_insta_Recientes.pivot(index='fecha', columns='dia', values='sentimiento')
+instr = sns.heatmap(instaRecientes, vmin=-1, vmax=1)
+instr.figure.savefig("HeatMapPostRecientesPorDia.png")
+
+
+
+
+
+
 
